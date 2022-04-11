df72227a2d890f33f74dae0d0b3f58e280f7edfe
Juliette Cordor <grassojames5@gmail.com>
2022-04-11 18:16:59 +1000

    use baseurl and updated styles

---
+++ src/components/PFP/index.tsx
@@ -0,0 +1,15 @@
+import type { FunctionComponent } from 'react';
+import Image from 'next/image';
+import styles from './PFP.module.scss';
+
+const PFP: FunctionComponent = () => (
+  <Image
+    src="https://avatars.githubusercontent.com/u/61040004"
+    width="200px"
+    height="200px"
+    alt="My Github profile picture"
+    className={styles.pfp}
+  />
+);
+
+export default PFP;
