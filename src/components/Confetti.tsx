92c747b2eabffdce4daf36a51082f19b4332566c
Juliette Cordor <grassojames5@gmail.com>
2022-04-11 17:51:04 +1000

    fixes

---
+++ src/components/Confetti.tsx
@@ -0,0 +1,49 @@
+import { type FunctionComponent, useEffect } from 'react';
+import confetti from 'canvas-confetti';
+
+/**
+ * NOTE: Could have been made a hook, but was done this way to better support static rendering
+ */
+const Container: FunctionComponent = () => {
+  useEffect(() => {
+    const colors = [
+      '#26ccff',
+      '#a25afd',
+      '#ff5e7e',
+      '#88ff5a',
+      '#fcff42',
+      '#ffa62d',
+      '#ff36ff',
+    ];
+    const baseOptions = {
+      particleCount: colors.length,
+      colors,
+    };
+
+    const end = Date.now() + 5 * 1000;
+
+    const frame = () => {
+      confetti({
+        ...baseOptions,
+        angle: 60,
+        spread: 55,
+        origin: { x: 0, y: 0.6 },
+      });
+      confetti({
+        ...baseOptions,
+        angle: 120,
+        spread: 55,
+        origin: { x: 1, y: 0.6 },
+      });
+      if (Date.now() < end) {
+        requestAnimationFrame(frame);
+      }
+    };
+
+    frame();
+  }, []);
+
+  return <></>;
+};
+
+export default Container;
