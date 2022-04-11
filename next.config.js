a0ce005321c1317c833b5490fa307299660f9a7b
Juliette Cordor <grassojames5@gmail.com>
2022-04-11 16:57:04 +1000

    fixes

--- next.config.js
+++ next.config.js
@@ -1,2 +1,22 @@
-require("ts-node").register();
+require("ts-node").register({
+  compilerOptions: {
+    target: "ES6",
+    allowJs: true,
+    skipLibCheck: true,
+    strict: true,
+    strictNullChecks: true,
+    forceConsistentCasingInFileNames: true,
+    noEmit: true,
+    noUncheckedIndexedAccess: true,
+    esModuleInterop: true,
+    module: "CommonJS",
+    resolveJsonModule: true,
+    isolatedModules: true,
+    incremental: true,
+    downlevelIteration: true,
+    sourceMap: false,
+    inlineSourceMap: false,
+  },
+});
+
 module.exports = require("./config/next.config").default;
