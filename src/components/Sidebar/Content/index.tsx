59ba68cbbafd252738d1b7618986bea5b51373ce
Juliette Cordor <grassojames5@gmail.com>
2022-04-19 14:24:28 +1000

    fixes

--- src/components/Sidebar/Content/index.tsx
+++ src/components/Sidebar/Content/index.tsx
@@ -4,7 +4,7 @@ import styles from './index.module.scss';
 const SidebarContent: FunctionComponent = () => {
   return (
     <div className={styles.container}>
-      <h1>Slayy</h1>
+      <h1>Sidebar Coming Soon...</h1>
     </div>
   );
 };
