23eff9e2bdc4e6d826d39de1fd66c22c7ccfcd4d
Juliette Cordor <grassojames5@gmail.com>
2022-04-19 14:23:51 +1000

    fixes

---
+++ src/components/Sidebar/Content/index.tsx
@@ -0,0 +1,12 @@
+import type { FunctionComponent } from 'react';
+import styles from './index.module.scss';
+
+const SidebarContent: FunctionComponent = () => {
+  return (
+    <div className={styles.container}>
+      <h1>Slayy</h1>
+    </div>
+  );
+};
+
+export default SidebarContent;
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,9 @@
 import { type FunctionComponent, useState } from 'react';
 import { FaBars } from 'react-icons/fa';
+import dynamic from 'next/dynamic';
 import styles from './index.module.scss';
+
+const SidebarContent = dynamic(() => import('components/Sidebar/Content'));
 
 const Sidebar: FunctionComponent = () => {
   const [open, setOpen] = useState(false);
@@ -8,8 +11,10 @@ const Sidebar: FunctionComponent = () => {
   return (
     <div className={styles.container}>
       <FaBars
+        onClick={() => setOpen(!open)}
         className={`${styles.hamburger} ${open ? styles.open : styles.closed}`}
       />
+      {open && <SidebarContent />}
     </div>
   );
 };
