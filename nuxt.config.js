38ab571f422a69da4ed7637566fab8ddfad1238a
Juliette Cordor <grassojames5@gmail.com>
2022-06-27 16:54:05 +1000

    fix: fixed static gen

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,10 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  image: {
+    domains: ['avatars.githubusercontent.com'],
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
