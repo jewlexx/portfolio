95c8efb49f72892befe3e1ea19fd32f46209c18f
Juliette Cordor <grassojames5@gmail.com>
2022-08-11 22:37:54 +1000

    added vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  integrations: [preact()],
+  adapter: vercel()
 });
