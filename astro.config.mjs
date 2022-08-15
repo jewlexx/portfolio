f9217ce5d5286ab8ac87e6cfa529d827e02aa330
Juliette Cordor <grassojames5@gmail.com>
2022-08-15 22:21:55 +1000

    added prettierrc

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import vercel from "@astrojs/vercel/serverless";
-
-import vue from "@astrojs/vue";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
   adapter: vercel(),
-  output: "server"
+  output: 'server',
 });
