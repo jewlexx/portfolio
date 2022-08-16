31998430c9d4053aeabb846db2f2451a3d180570
Juliette Cordor <grassojames5@gmail.com>
2022-08-16 10:48:26 +1000

    fix

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="button">{{ text }}</span>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    text: String
+})
+</script>
+
+<style lang="scss" scoped>
+.button {
+    display: inline-block;
+    border: 3px solid currentColor;
+    padding: 0.5em 1em;
+    font-weight: 700;
+    text-transform: uppercase;
+}
+</style>
