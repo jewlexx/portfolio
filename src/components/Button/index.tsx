5883a52e2725ae9993077bf66be5fbf04dacc2d7
Juliette Cordor <grassojames5@gmail.com>
2022-08-16 10:50:57 +1000

    removed old button

--- src/components/Button/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { FunctionComponent } from 'preact';
-import Styles from './styles.module.scss';
-
-const Button: FunctionComponent = ({ children }) => {
-  return <span className={Styles.button}>{children}</span>;
-};
-
-export default Button;
