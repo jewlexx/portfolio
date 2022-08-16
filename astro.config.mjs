a52830b36330e03893ecede4f2e7a7fbb2f7d6e1
Juliette Cordor <grassojames5@gmail.com>
2022-08-16 10:30:58 +1000

    added integrations

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import vercel from '@astrojs/vercel/serverless';
+import vue from "@astrojs/vue";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact()],
+  integrations: [preact(), vue(), prefetch()],
   adapter: vercel(),
-  output: 'server',
+  output: 'server'
 });
