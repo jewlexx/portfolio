52f18b670404abe5916d5f309d0716c9605ae8b8
Juliette Cordor <grassojames5@gmail.com>
2022-08-19 20:07:34 +1000

    moved back to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,12 @@ import preact from '@astrojs/preact';
 import vercel from '@astrojs/vercel/serverless';
 import vue from "@astrojs/vue";
 import prefetch from "@astrojs/prefetch";
-import deno from "@astrojs/deno";
-
 import mdx from "@astrojs/mdx";
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), vue(), prefetch(), mdx()],
-  adapter: deno(),
+  adapter: vercel(),
   output: 'server'
 });
