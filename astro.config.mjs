ad828af75b7b802e038fb6824412dcae6e300cf3
Juliette Cordor <grassojames5@gmail.com>
2022-08-19 21:04:42 +1000

    moved to static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import vercel from '@astrojs/vercel/serverless';
 import vue from "@astrojs/vue";
 import prefetch from "@astrojs/prefetch";
 import mdx from "@astrojs/mdx";
@@ -9,6 +8,6 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), vue(), prefetch(), mdx()],
-  adapter: vercel(),
-  output: 'server'
+  // adapter: vercel(),
+  // output: 'static'
 });
