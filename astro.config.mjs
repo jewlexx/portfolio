1ec43df3e6f32a07c8348ddfcb1df6b5bd2654fd
Juliette Cordor <grassojames5@gmail.com>
2022-08-19 19:57:53 +1000

    added deno and mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import vercel from '@astrojs/vercel/serverless';
 import vue from "@astrojs/vue";
-
 import prefetch from "@astrojs/prefetch";
+import deno from "@astrojs/deno";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), vue(), prefetch()],
-  adapter: vercel(),
+  integrations: [preact(), vue(), prefetch(), mdx()],
+  adapter: deno(),
   output: 'server'
 });
