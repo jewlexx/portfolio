6685164dd447465ed33e85b5d5054395388eea7f
Juliette Cordor <grassojames5@gmail.com>
2022-08-24 14:18:04 +1000

    updates and fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import vue from "@astrojs/vue";
-import prefetch from "@astrojs/prefetch";
-import mdx from "@astrojs/mdx";
-
+import vue from '@astrojs/vue';
+import prefetch from '@astrojs/prefetch';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), vue(), prefetch(), mdx()],
+  integrations: [preact(), vue(), prefetch()],
   // adapter: vercel(),
   // output: 'static'
 });
