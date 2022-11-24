80efc110407709367eaebbc911d430a1f07033f2
Juliette Cordor <grassojames5@gmail.com>
2022-11-24 18:21:35 +1100

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,31 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.scss';
 
+interface Link {
+  name: string;
+  url: string;
+  emoji: string;
+}
+
 const Home: NextPage = () => {
+  const links: Link[] = [
+    {
+      emoji: '👩‍💻',
+      name: 'Github',
+      url: 'github.com/jewlexx',
+    },
+    {
+      emoji: '🕊️',
+      name: 'Twitter',
+      url: 'twitter.com/jewelexx',
+    },
+    {
+      emoji: '🌲',
+      name: 'Linktree',
+      url: 'linktr.ee/jewelexx',
+    },
+  ];
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,59 +36,31 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+      <main>
+        <header className={styles.hero}>
+          <div className={styles.overlay}>
+            <h1 className={styles.title}>
+              Juliette Cordor
+              <small className={styles.subtitle}>
+                <span className={styles.pronouns}>(She/Her)</span>
+              </small>
+            </h1>
+            <div>
+              {links.map(({ emoji, name, url }) => (
+                <a
+                  key={`${emoji}-${name}-${url}`}
+                  href={`https://${url}`}
+                  className="role"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {emoji} {name}
+                </a>
+              ))}
+            </div>
+          </div>
+        </header>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
