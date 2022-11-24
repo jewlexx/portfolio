bf7fa86bd2abc794bc4bc14351966d50952a0fae
Juliette Cordor <grassojames5@gmail.com>
2022-11-24 18:27:00 +1100

    fix

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -1,33 +1,39 @@
 import { FunctionComponent } from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+
+import styles from './index.module.scss';
 
 const Nav: FunctionComponent = () => {
   return (
-    <nav className="nav">
-      <a className="logolink" href="/" rel="prefetch">
-        <img
+    <nav className={styles.nav}>
+      <Link className={styles.logolink} href="/" rel="prefetch">
+        <Image
           alt="Personal Profile Picture"
-          className="monogram"
-          src="/assets/pfp-min.jpg"
+          className={styles.monogram}
+          src="/pfp.jpg"
+          width={128}
+          height={128}
         />
-      </a>
-      <a className="link" href="/projects">
+      </Link>
+      <a className={styles.link} href="/projects">
         Portfolio
       </a>
-      <a className="link" href="/about">
+      <a className={styles.link} href="/about">
         About
       </a>
-      <a className="social" href="https://twitter.com/jewelexx">
+      <a className={styles.social} href="https://twitter.com/jewelexx">
         <svg
-          className="socialicon"
+          className={styles.socialicon}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
         >
           <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334 0-.14 0-.282-.006-.422A6.685 6.685 0 0 0 16 3.542a6.658 6.658 0 0 1-1.889.518 3.301 3.301 0 0 0 1.447-1.817 6.533 6.533 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.325 9.325 0 0 1-6.767-3.429 3.289 3.289 0 0 0 1.018 4.382A3.323 3.323 0 0 1 .64 6.575v.045a3.288 3.288 0 0 0 2.632 3.218 3.203 3.203 0 0 1-.865.115 3.23 3.23 0 0 1-.614-.057 3.283 3.283 0 0 0 3.067 2.277A6.588 6.588 0 0 1 .78 13.58a6.32 6.32 0 0 1-.78-.045A9.344 9.344 0 0 0 5.026 15z" />
         </svg>
       </a>
-      <a className="social" href="https://github.com/jewlexx">
+      <a className={styles.social} href="https://github.com/jewlexx">
         <svg
-          className="socialicon"
+          className={styles.socialicon}
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
         >
