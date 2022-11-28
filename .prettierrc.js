802aee03ca0f8cd7252cca1eaa3b25102e8e26f2
Juliette Cordor <grassojames5@gmail.com>
2022-11-29 10:27:33 +1100

    added prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+/** @type {import('prettier').Config} */
+const config = {
+  singleQuote: true,
+  tabWidth: 2,
+  endOfLine: 'lf',
+  quoteProps: 'as-needed',
+  semi: true,
+};
+
+module.exports = config;
