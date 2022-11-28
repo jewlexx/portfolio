d8bb6f67311c91b5295dbaa14ac39991c8a17e97
Juliette Cordor <grassojames5@gmail.com>
2022-11-28 18:09:37 +1100

    added document stuff

--- next.config.js
+++ next.config.js
@@ -1,7 +1,68 @@
+const { favicons } = require('favicons');
+const fs = require('fs');
+
+// Create favicons
+const configuration = {
+  path: '/',
+  appName: 'Juliette Cordor',
+  appDescription: 'Juliette Cordor, developer and other things also',
+  developerName: 'Juliette Cordor',
+  developerURL: 'https://github.com/jewlexx',
+  dir: 'auto', // Primary text direction for name, short_name, and description
+  lang: 'en-US', // Primary language for name and short_name
+  background: '#fff', // Background colour for flattened icons. `string`
+  theme_color: '#fff', // Theme color user for example in Android's task switcher. `string`
+  appleStatusBarStyle: 'black-translucent', // Style for Apple status bar: "black-translucent", "default", "black". `string`
+  display: 'standalone', // Preferred display mode: "fullscreen", "standalone", "minimal-ui" or "browser". `string`
+  orientation: 'any', // Default orientation: "any", "natural", "portrait" or "landscape". `string`
+  scope: '/', // set of URLs that the browser considers within your app
+  start_url: '/?homescreen=1', // Start URL when launching the application from a device. `string`
+  preferRelatedApplications: false, // Should the browser prompt the user to install the native companion app. `boolean`
+  relatedApplications: undefined, // Information about the native companion apps. This will only be used if `preferRelatedApplications` is `true`. `Array<{ id: string, url: string, platform: string }>`
+  pixel_art: false, // Keeps pixels "sharp" when scaling up, for pixel art.  Only supported in offline mode.
+  loadManifestWithCredentials: false, // Browsers don't send cookies when fetching a manifest, enable this to fix that. `boolean`
+  manifestMaskable: false, // Maskable source image(s) for manifest.json. "true" to use default source. More information at https://web.dev/maskable-icon/. `boolean`, `string`, `buffer` or array of `string`
+  icons: {
+    // Platform Options:
+    // - offset - offset in percentage
+    // - background:
+    //   * false - use default
+    //   * true - force use default, e.g. set background for Android icons
+    //   * color - set background for the specified icons
+    //
+    android: true, // Create Android homescreen icon. `boolean` or `{ offset, background }` or an array of sources
+    appleIcon: true, // Create Apple touch icons. `boolean` or `{ offset, background }` or an array of sources
+    appleStartup: true, // Create Apple startup images. `boolean` or `{ offset, background }` or an array of sources
+    favicons: true, // Create regular favicons. `boolean` or `{ offset, background }` or an array of sources
+    windows: true, // Create Windows 8 tile icons. `boolean` or `{ offset, background }` or an array of sources
+    yandex: true, // Create Yandex browser icon. `boolean` or `{ offset, background }` or an array of sources
+  },
+};
+
+(async () => {
+  const response = await favicons('public/assets/pfp.jpg');
+
+  if (!fs.existsSync('public/icons')) {
+    fs.mkdirSync('public/icons');
+  }
+
+  response.images.forEach((image) => {
+    fs.writeFileSync(`public/icons/${image.name}`, image.contents);
+  });
+
+  response.files.forEach((file) => {
+    fs.writeFileSync(`public/${file.name}`, file.contents);
+  });
+
+  console.log(
+    `Add the following to \`_document.tsx\` ${response.html.join('\n')}`,
+  );
+})();
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
