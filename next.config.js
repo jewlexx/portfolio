fb36c1ed2e12a42f7c05cce58fe56c4cde8ffc38
Juliette Cordor <grassojames5@gmail.com>
2022-11-28 18:16:28 +1100

    fixed favicon generation

--- next.config.js
+++ next.config.js
@@ -47,11 +47,6 @@ const configuration = {
   }
 
   response.images.forEach((image) => {
-    // Only write favicon to root public dir
-    if (image.name === 'favicon.ico') {
-      fs.writeFileSync(`public/${image.name}`, image.contents);
-    }
-
     fs.writeFileSync(`public/icons/${image.name}`, image.contents);
   });
 
