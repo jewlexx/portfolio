c00f347fc266e9dc72cb5a9c3a76c518ea56a8db
Juliette Cordor <grassojames5@gmail.com>
2022-11-28 18:58:23 +1100

    added sun svg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { motion } from 'framer-motion';
 import Nav from '../components/Nav';
 import styles from '../styles/Home.module.scss';
 
@@ -66,6 +67,37 @@ const Home: NextPage = () => {
           </div>
         </header>
       </main>
+
+      {/* Downloaded from https://www.svgrepo.com/ under the CC0 license as detailed here: https://www.svgrepo.com/page/licensing */}
+      <motion.svg x="0px" y="0px" viewBox="0 0 473.931 473.931">
+        <circle
+          style={{ fill: '#F2BE3E' }}
+          cx="236.966"
+          cy="236.966"
+          r="236.966"
+        />
+        <circle
+          style={{ fill: '#F1EB75' }}
+          cx="236.966"
+          cy="236.966"
+          r="117.154"
+        />
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+        <g></g>
+      </motion.svg>
     </div>
   );
 };
