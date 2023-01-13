c5355a74a4f2e29e68f2d2c54df1b62ce3fa5bf2
Juliette Cordor <grassojames5@gmail.com>
2023-01-14 00:25:25 +1100

    added basic stuff

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,77 @@
-import './globals.css'
+import {
+  type TablerIcon,
+  IconBrandGithub,
+  IconBrandTwitch,
+  IconBrandTwitter,
+  IconBrandLinktree,
+} from '@tabler/icons';
+import Image from 'next/image';
+import pfp from '../public/pfp.avif';
+import styles from './layout.module.scss';
+import './globals.css';
+
+interface Link {
+  name: string;
+  url: string;
+  emoji: TablerIcon;
+}
+
+const links: Link[] = [
+  {
+    emoji: IconBrandGithub,
+    name: 'Github',
+    url: 'https://github.com/jewlexx',
+  },
+  {
+    emoji: IconBrandTwitch,
+    name: 'Twitch',
+    url: 'https://twitch.tv/sapphicjewl',
+  },
+  {
+    emoji: IconBrandTwitter,
+    name: 'Twitter',
+    url: 'https://twitter.com/jewelexx',
+  },
+  {
+    emoji: IconBrandLinktree,
+    name: 'Linktree',
+    url: 'https://linktr.ee/jewelexx',
+  },
+];
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header>
+          <nav className={styles.nav}>
+            <>
+              <a className={styles.logolink} href="/" rel="prefetch">
+                <Image
+                  alt="Profile"
+                  className={styles.monogram}
+                  src={pfp}
+                  width={32}
+                  height={32}
+                />
+              </a>
+              {/* <a class="link" href="/projects">Portfolio</a>
+		<a class="link" href="/about">About</a> */}
+              {links.map((link) => (
+                <a className={styles.social} href={link.url} key={link.name}>
+                  <link.emoji className={styles.socialicon} />
+                </a>
+              ))}
+            </>
+          </nav>
+        </header>
+        <div className={styles.app}>{children}</div>
+      </body>
     </html>
-  )
+  );
 }
