b3f64b16f3290948dc4072c927b3d1cb2e2489b2
Juliette Cordor <grassojames5@gmail.com>
2023-01-14 23:43:40 +1100

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -5,5 +5,26 @@ import styles from './page.module.css';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (<main className={styles.main}>
+    <header class="hero">
+	<div class="overlay">
+		<h1 class="title">
+			Juliette Cordor
+			<small class="pronouns"> (She/Her) </small>
+		</h1>
+        <div>
+			{#each links as { emoji, name, url }}
+				<a
+					href={`https://${url}`}
+					target="_blank"
+					rel="noopener noreferrer"
+					class={`role ${true ? 'motion' : ''}`}
+				>
+					<img src={emoji} alt={name} />
+				</a>
+			{/each}
+		</div>
+	</div>
+</header>
+  </main>);
 }
