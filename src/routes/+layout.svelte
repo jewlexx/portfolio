20ea83ec6d80adf7544681da46cb77537d4b12b9
Juliette Cordor <grassojames5@gmail.com>
2023-02-23 15:27:28 +1100

    updates and fixes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -19,12 +19,10 @@
 	}
 
 	main {
-		flex: 1;
 		display: flex;
 		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
+		width: 100vw;
+		height: 90vh;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
