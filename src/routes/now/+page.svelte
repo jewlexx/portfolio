8321630c8e713e193959bff3737151b04521c20f
Juliette Cordor <grassojames5@gmail.com>
2023-02-23 17:42:48 +1100

    moved css to tailwind

--- src/routes/now/+page.svelte
+++ src/routes/now/+page.svelte
@@ -13,18 +13,7 @@
 	}
 </script>
 
-<div class="main">
+<div class="flex flex-col items-center justify-center min-h-full min-w-full">
 	<h1 class="font-bold text-5xl sm:text-8xl text-green-500 dark:text-green-400">{time}</h1>
 	<p class="text-2xl">seconds since 01/01/1970 (UTC)</p>
 </div>
-
-<style lang="scss">
-	.main {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		flex-direction: column;
-		min-height: 100%;
-		min-width: 100%;
-	}
-</style>
