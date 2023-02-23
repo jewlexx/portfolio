765977b704c7d37037fd3c1aab8acd2aee3d4b0d
Juliette Cordor <grassojames5@gmail.com>
2023-02-23 17:12:23 +1100

    updates and fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,37 +7,9 @@
 	/>
 </svelte:head>
 
-<header class="hero">
+<header>
 	<div class="flex flex-col pl-8 justify-center items-start w-full h-full absolute overlay">
-		<h1 class="title text-sm break-words">Juliette Cordor</h1>
-		<!-- <small class="pronouns text-3xl text-cambridge-blue"> (She/Her) </small> -->
+		<h1 class="title text-4xl sm:text-7xl break-words">Juliette Cordor</h1>
+		<small class="pronouns text-3xl text-cambridge-blue"> (She/Her) </small>
 	</div>
 </header>
-
-<style lang="scss">
-	@import '$lib/styles/palette.scss';
-
-	.hero {
-		position: relative;
-		overflow: hidden;
-		@media (min-width: $w-s) {
-			height: 45vw;
-		}
-	}
-
-	.overlay {
-		@media (min-width: $w-s) {
-			padding-left: 4rem;
-		}
-	}
-
-	.title {
-		font-weight: 900;
-		margin-bottom: 1rem;
-		margin-top: 0;
-
-		@media (min-width: 1024px) {
-			font-size: $f-u12;
-		}
-	}
-</style>
