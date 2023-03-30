36e9109352d150b28a4cd116a861f12f418b2780
Juliette Cordor <juliette@jewelexx.com>
2023-03-30 21:58:44 +1100

    removed unused imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import { usePageSize } from '@/hooks/size';
-import { useMemo } from 'react';
-import { useMousePosition } from '@/hooks/pos';
 import styles from '@/styles/Home.module.scss';
 
 const inter = Inter({ subsets: ['latin'] });
