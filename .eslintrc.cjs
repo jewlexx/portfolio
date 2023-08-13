c5eeab75ef03d0eccc2bd18462229781f4e09a09
Juliette Cordor <juliette@jewelexx.com>
2023-08-13 19:35:40 +1000

    updated configs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
 	root: true,
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:svelte/recommended',
+		'plugin:@typescript-eslint/recommended',
+		'prettier'
+	],
+	// plugins: ['svelte', '@typescript-eslint'],
 	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
+	// overrides: [{ files: ['*.svelte'], processor: 'svelte/svelte' }],
+	// settings: {
+	// 	'svelte/typescript': () => require('typescript')
+	// },
 	parserOptions: {
 		sourceType: 'module',
 		ecmaVersion: 2020
