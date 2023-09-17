25fbcbc867457f37149cd4497462e2a8e412bf9c
Juliette Cordor <juliette@jewelexx.com>
2023-09-17 22:20:09 +1000

    use such a cool mirror selfie wow

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Icon from 'svelte-fa';
 	import { links } from '$lib/links';
-	import ProfilePicture from '$lib/assets/images/pfp.jpg';
+	import ProfilePicture from '$lib/assets/images/mirror selfie.webp';
 </script>
 
 <header>
