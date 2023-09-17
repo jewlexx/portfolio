ed7ff2fa33b7ba888edacd02f92267efeb3ac454
Juliette Cordor <juliette@jewelexx.com>
2023-09-17 22:59:51 +1000

    move links into header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,6 +1,6 @@
 <script>
-	import { links } from '$lib/links';
 	import ProfilePicture from '$lib/assets/images/mirror selfie.webp?w=32&h=32';
+	import { links } from '$lib/links';
 </script>
 
 <header>
@@ -10,11 +10,13 @@
 		</a>
 		<!-- <a class="link" href="/projects">Portfolio</a>
 		<a class="link" href="/about">About</a> -->
-		{#each links as { emoji: Emoji, url }}
-			<a class="social" href={url}>
-				<Emoji class="socialicon" />
-			</a>
-		{/each}
+		<span class="links-container">
+			{#each links as { emoji: Emoji, url }}
+				<a href={url} target="_blank" rel="noopener noreferrer" class="role">
+					<Emoji />
+				</a>
+			{/each}
+		</span>
 	</nav>
 </header>
 
@@ -25,6 +27,7 @@
 		background-color: lighten($dark-gray, 10%);
 		display: flex;
 		align-items: center;
+		justify-content: space-between;
 		padding-top: 1rem;
 		padding-right: 2rem;
 		padding-bottom: 1rem;
@@ -66,20 +69,99 @@
 		border-radius: 50%;
 	}
 
-	.social {
-		display: block;
-		margin-left: auto;
+	.links-container {
+		display: flex;
+	}
 
+	.role {
+		text-decoration: none;
+		position: relative;
+		font-weight: 900;
+		color: $t-bg;
+		background-color: $t-fg;
+		padding: 0.25em;
+		z-index: 2;
+		border-radius: 0.25rem;
+
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		@media (min-width: $w-s) {
+			font-size: $f-u3;
+		}
+
+		+ .role {
+			margin-left: 0.5em;
+		}
+
+		// Only enable motion if the user has no preference
 		@media (prefers-reduced-motion: no-preference) {
-			transition: font-size 50ms;
+			transition: transform 500ms
+				linear(
+					0 0%,
+					0.22 2.1%,
+					0.86 6.5%,
+					1.11 8.6%,
+					1.3 10.7%,
+					1.35 11.8%,
+					1.37 12.9%,
+					1.37 13.7%,
+					1.36 14.5%,
+					1.32 16.2%,
+					1.03 21.8%,
+					0.94 24%,
+					0.89 25.9%,
+					0.88 26.85%,
+					0.87 27.8%,
+					0.87 29.25%,
+					0.88 30.7%,
+					0.91 32.4%,
+					0.98 36.4%,
+					1.01 38.3%,
+					1.04 40.5%,
+					1.05 42.7%,
+					1.05 44.1%,
+					1.04 45.7%,
+					1 53.3%,
+					0.99 55.4%,
+					0.98 57.5%,
+					0.99 60.7%,
+					1 68.1%,
+					1.01 72.2%,
+					1 86.7%,
+					1 100%
+				);
 		}
 
 		&:hover {
-			font-size: $f-u1;
-		}
+			color: $t-fg;
 
-		+ .social {
-			margin-left: 0.75rem;
+			transform: scale(1.2);
+
+			&:nth-of-type(1) {
+				$color: $black;
+				background-color: $color;
+				box-shadow: 0px 0px 3px #000 inset;
+			}
+
+			&:nth-of-type(2) {
+				$color: $purple;
+				background-color: $color;
+				box-shadow: 0px 0px 3px #000 inset;
+			}
+
+			&:nth-of-type(3) {
+				$color: $blue;
+				background-color: $color;
+				box-shadow: 0px 0px 3px #000 inset;
+			}
+
+			&:nth-of-type(4) {
+				$color: $green;
+				background-color: $color;
+				box-shadow: 0px 0px 3px #000 inset;
+			}
 		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-	import { links } from '$lib/links';
-</script>
-
 <svelte:head>
 	<title>Juliette Cordor</title>
 	<meta
@@ -17,13 +13,6 @@
 			Juliette Cordor
 			<small class="pronouns"> (She/Her) </small>
 		</h1>
-		<div class="links-container">
-			{#each links as { emoji: Emoji, url }}
-				<a href={url} target="_blank" rel="noopener noreferrer" class="role">
-					<Emoji />
-				</a>
-			{/each}
-		</div>
 	</div>
 </header>
 
@@ -74,102 +63,6 @@
 
 		@media (min-width: 1024px) {
 			font-size: $f-u12;
-		}
-	}
-
-	.links-container {
-		display: flex;
-	}
-
-	.role {
-		text-decoration: none;
-		position: relative;
-		font-weight: 900;
-		color: $t-bg;
-		background-color: $t-fg;
-		padding: 0.25em;
-		z-index: 2;
-		border-radius: 0.25rem;
-
-		display: flex;
-		align-items: center;
-		justify-content: center;
-
-		@media (min-width: $w-s) {
-			font-size: $f-u3;
-		}
-
-		+ .role {
-			margin-left: 0.5em;
-		}
-
-		// Only enable motion if the user has no preference
-		@media (prefers-reduced-motion: no-preference) {
-			transition: transform 500ms
-				linear(
-					0 0%,
-					0.22 2.1%,
-					0.86 6.5%,
-					1.11 8.6%,
-					1.3 10.7%,
-					1.35 11.8%,
-					1.37 12.9%,
-					1.37 13.7%,
-					1.36 14.5%,
-					1.32 16.2%,
-					1.03 21.8%,
-					0.94 24%,
-					0.89 25.9%,
-					0.88 26.85%,
-					0.87 27.8%,
-					0.87 29.25%,
-					0.88 30.7%,
-					0.91 32.4%,
-					0.98 36.4%,
-					1.01 38.3%,
-					1.04 40.5%,
-					1.05 42.7%,
-					1.05 44.1%,
-					1.04 45.7%,
-					1 53.3%,
-					0.99 55.4%,
-					0.98 57.5%,
-					0.99 60.7%,
-					1 68.1%,
-					1.01 72.2%,
-					1 86.7%,
-					1 100%
-				);
-		}
-
-		&:hover {
-			color: $t-fg;
-
-			transform: scale(1.2);
-
-			&:nth-of-type(1) {
-				$color: $black;
-				background-color: $color;
-				box-shadow: 0px 0px 3px #000 inset;
-			}
-
-			&:nth-of-type(2) {
-				$color: $purple;
-				background-color: $color;
-				box-shadow: 0px 0px 3px #000 inset;
-			}
-
-			&:nth-of-type(3) {
-				$color: $blue;
-				background-color: $color;
-				box-shadow: 0px 0px 3px #000 inset;
-			}
-
-			&:nth-of-type(4) {
-				$color: $green;
-				background-color: $color;
-				box-shadow: 0px 0px 3px #000 inset;
-			}
 		}
 	}
 </style>
