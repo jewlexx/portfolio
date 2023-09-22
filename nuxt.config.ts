e436f981c277ed4e0d90a1aa3822420afdfcc3f7
Juliette Cordor <juliette@jewelexx.com>
2023-09-22 16:44:58 +1000

    update seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,11 @@
 export default defineNuxtConfig({
     site: {
         url: 'https://www.jewelexx.com',
+        name: "Juliette Cordor's Portfolio",
+        description: 'Juliette Cordor, developer and other things also.',
+        defaultLocale: 'en',
     },
     devtools: { enabled: true },
     css: ['@/scss/global.scss'],
-    modules: ['nuxt-simple-sitemap', 'nuxt-simple-robots'],
+    modules: ['nuxt-simple-sitemap', 'nuxt-simple-robots', '@nuxtseo/module'],
 });
