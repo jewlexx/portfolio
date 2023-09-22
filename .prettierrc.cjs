9cc656c2d3ef90e80d9878f0d50ce0b6159870b5
Juliette Cordor <juliette@jewelexx.com>
2023-09-22 16:20:58 +1000

    fixes

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,9 +1,8 @@
-/** @type {import('prettier').Config} */
-module.exports = {
-	parser: 'vue',
-	tabWidth: 4,
-	useTabs: false,
-	singleQuote: true,
-	trailingComma: 'none',
-	printWidth: 100
+/** @type {import('prettier').Config} */ module.exports = {
+    tabWidth: 4,
+    useTabs: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    printWidth: 100,
+    semi: true
 };
--- utils/links.ts
+++ utils/links.ts
@@ -1,8 +1,22 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */ import { IconBrandGithub, IconBrandTwitch,
-IconBrandX as IconBrandTwitter, IconBrandLinktree } from '@tabler/icons-vue'; // export enum Icon {
-// Github, // Twitch, // Twitter, // } export interface Link { title: string; url: string; emoji:
-typeof IconBrandGithub; } export const links: Link[] = [ { emoji: IconBrandGithub, title: 'GitHub',
-url: 'https://github.com/jewlexx' }, { emoji: IconBrandTwitch, title: 'Twitch', url:
-'https://twitch.tv/possiblyjuliette' }, { emoji: IconBrandTwitter, title: 'X (Twitter)', url:
-'https://twitter.com/jewelexx' }, { emoji: IconBrandLinktree, title: 'Linktree', url:
-'https://linktr.ee/jewelexx' } ];
+/* eslint-disable @typescript-eslint/no-explicit-any */ import {
+    IconBrandGithub,
+    IconBrandTwitch,
+    IconBrandX as IconBrandTwitter,
+    IconBrandLinktree
+} from '@tabler/icons-vue';
+
+// export enum Icon {
+// Github, // Twitch, // Twitter, // }
+
+export interface Link {
+    title: string;
+    url: string;
+    emoji: typeof IconBrandGithub;
+}
+
+export const links: Link[] = [
+    { emoji: IconBrandGithub, title: 'GitHub', url: 'https://github.com/jewlexx' },
+    { emoji: IconBrandTwitch, title: 'Twitch', url: 'https://twitch.tv/possiblyjuliette' },
+    { emoji: IconBrandTwitter, title: 'X (Twitter)', url: 'https://twitter.com/jewelexx' },
+    { emoji: IconBrandLinktree, title: 'Linktree', url: 'https://linktr.ee/jewelexx' }
+];
