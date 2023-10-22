43cd90c4b2e34e7bb1bb535b77883771dd13b683
Juliette Cordor <juliette@jewelexx.com>
2023-10-22 17:21:06 +1100

    transform repo url

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,35 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const GITHUB_USER = 'jewlexx';
+
+const repoToUrl = (arg: string | undefined, ctx: z.RefinementCtx): string | undefined => {
+	if (!arg) {
+		return undefined;
+	}
+
+	if (arg.startsWith('https')) {
+		return arg;
+	}
+	const repoPath = arg.split('/', 2);
+	if (repoPath.length > 1) {
+		const [user, repo] = repoPath;
+		return `https://github.com/${user}/${repo}`;
+	} else {
+		const [repo] = repoPath;
+		return `https://github.com/${GITHUB_USER}/${repo}`;
+	}
+
+}
+
+const projects = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		repo: z.string().optional().transform(repoToUrl),
+		homepage: z.string().optional(),
 	}),
 });
 
-export const collections = { blog };
+export const collections = { projects };
