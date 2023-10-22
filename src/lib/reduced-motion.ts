c34cdf7228c9b85f2ae07731adfe0588812e84e5
Juliette Cordor <juliette@jewelexx.com>
2023-10-22 17:35:31 +1100

    fixes

--- src/lib/reduced-motion.ts
+++ src/lib/reduced-motion.ts
@@ -1,23 +1,24 @@
-import { readable } from 'svelte/store';
-import { browser } from '$app/environment';
+// TODO: Implement this
+// import { readable } from 'svelte/store';
+// import { browser } from '$app/environment';
 
-const reducedMotionQuery = '(prefers-reduced-motion: reduce)';
+// const reducedMotionQuery = '(prefers-reduced-motion: reduce)';
 
-const getInitialMotionPreference = () => {
-	if (!browser) return false;
-	return window.matchMedia(reducedMotionQuery).matches;
-};
+// const getInitialMotionPreference = () => {
+// 	if (!browser) return false;
+// 	return window.matchMedia(reducedMotionQuery).matches;
+// };
 
-export const reducedMotion = readable(getInitialMotionPreference(), (set) => {
-	if (browser) {
-		const set_reduced_motion = (event: MediaQueryListEvent) => {
-			set(event.matches);
-		};
-		const media_query_list = window.matchMedia(reducedMotionQuery);
-		media_query_list.addEventListener('change', set_reduced_motion);
+// export const reducedMotion = readable(getInitialMotionPreference(), (set) => {
+// 	if (browser) {
+// 		const set_reduced_motion = (event: MediaQueryListEvent) => {
+// 			set(event.matches);
+// 		};
+// 		const media_query_list = window.matchMedia(reducedMotionQuery);
+// 		media_query_list.addEventListener('change', set_reduced_motion);
 
-		return () => {
-			media_query_list.removeEventListener('change', set_reduced_motion);
-		};
-	}
-});
+// 		return () => {
+// 			media_query_list.removeEventListener('change', set_reduced_motion);
+// 		};
+// 	}
+// });
