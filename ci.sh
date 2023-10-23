71a4a0b48ebbe9913b4cb926ddd4b5f59dda660a
Juliette Cordor <juliette@jewelexx.com>
2023-10-23 17:49:35 +1100

    added ci script

---
+++ ci.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+case $1 in
+"install")
+    echo "Installing Rustup..."
+    # Install Rustup (compiler)
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+    # Adding binaries to path
+    source "$HOME/.cargo/env"
+
+    echo "Installing wasm-pack..."
+    # Install wasm-pack
+    curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
+
+    echo "Installing dependencies..."
+    pnpm i
+    ;;
+
+"build")
+    echo "Building WebAssembly module..."
+    pnpm build:wasm
+
+    echo "Building web app..."
+    pnpm build
+    ;;
+esac
