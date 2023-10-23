e85a1027ae8587f0215648123174159632638ad5
Juliette Cordor <juliette@jewelexx.com>
2023-10-23 17:42:19 +1100

    added smiley face draw

--- stars/src/lib.rs
+++ stars/src/lib.rs
@@ -1,6 +1,12 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use web_sys::HtmlCanvasElement;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    utils::set_panic_hook();
+}
 
 #[wasm_bindgen]
 extern "C" {
@@ -10,4 +16,41 @@ extern "C" {
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, stars!");
+}
+
+#[wasm_bindgen]
+pub fn attach(canvas: HtmlCanvasElement) {
+    use std::f64;
+    let context = canvas
+        .get_context("2d")
+        .unwrap()
+        .unwrap()
+        .dyn_into::<web_sys::CanvasRenderingContext2d>()
+        .unwrap();
+
+    // Draw a smiley face
+    context.begin_path();
+
+    // Draw the outer circle.
+    context
+        .arc(75.0, 75.0, 50.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the mouth.
+    context.move_to(110.0, 75.0);
+    context.arc(75.0, 75.0, 35.0, 0.0, f64::consts::PI).unwrap();
+
+    // Draw the left eye.
+    context.move_to(65.0, 65.0);
+    context
+        .arc(60.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    // Draw the right eye.
+    context.move_to(95.0, 65.0);
+    context
+        .arc(90.0, 65.0, 5.0, 0.0, f64::consts::PI * 2.0)
+        .unwrap();
+
+    context.stroke();
 }
