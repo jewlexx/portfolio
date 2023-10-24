9eef9d0ba38faa33b6e6a49c077adbe1c983e66d
Juliette Cordor <juliette@jewelexx.com>
2023-10-24 15:46:13 +1100

    add astro image tools

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import svelte from '@astrojs/svelte';
 import robotsTxt from 'astro-robots-txt';
 import sitemap from '@astrojs/sitemap';
 import AstroPWA from '@vite-pwa/astro'
+import { astroImageTools } from "astro-imagetools";
+import Compress from "astro-compress";
+
 
 import wasm from 'vite-plugin-wasm';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.jewelexx.com',
-	integrations: [mdx(), sitemap(), svelte(), AstroPWA(), robotsTxt()],
+	integrations: [mdx(), sitemap(), svelte(), AstroPWA(), robotsTxt(), astroImageTools(), Compress()],
 	vite: {
 		plugins: [wasm()],
 		server: {
