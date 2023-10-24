79d658c2380f25591161573f654c5d57f8cfc3c3
Juliette Cordor <juliette@jewelexx.com>
2023-10-24 15:35:02 +1100

    added pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import mdx from '@astrojs/mdx';
 import svelte from '@astrojs/svelte';
 
 import sitemap from '@astrojs/sitemap';
+import AstroPWA from '@vite-pwa/astro'
 
 import wasm from 'vite-plugin-wasm';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://www.jewelexx.com',
-	integrations: [mdx(), sitemap(), svelte()],
+	integrations: [mdx(), sitemap(), svelte(), AstroPWA()],
 	vite: {
 		plugins: [wasm()],
 		server: {
