9002542fcd81bcbb916034d387471af08964e27d
Juliette Cordor <juliette@jewelexx.com>
2023-10-25 11:42:32 +1100

    fixes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -79,11 +79,54 @@
 			justify-content: center;
 
 			font-size: 2.5rem;
-			border: 5px solid black;
-			background-color: white;
+			/* border: 5px solid black; */
+			/* background-color: white; */
 			border-radius: 100%;
-			padding: 0.75rem;
+			/* padding: 0.25rem; */
 			margin-right: 1rem;
+
+			// Only enable motion if the user has no preference
+			@media (prefers-reduced-motion: no-preference) {
+				transition: transform 500ms
+					linear(
+						0 0%,
+						0.22 2.1%,
+						0.86 6.5%,
+						1.11 8.6%,
+						1.3 10.7%,
+						1.35 11.8%,
+						1.37 12.9%,
+						1.37 13.7%,
+						1.36 14.5%,
+						1.32 16.2%,
+						1.03 21.8%,
+						0.94 24%,
+						0.89 25.9%,
+						0.88 26.85%,
+						0.87 27.8%,
+						0.87 29.25%,
+						0.88 30.7%,
+						0.91 32.4%,
+						0.98 36.4%,
+						1.01 38.3%,
+						1.04 40.5%,
+						1.05 42.7%,
+						1.05 44.1%,
+						1.04 45.7%,
+						1 53.3%,
+						0.99 55.4%,
+						0.98 57.5%,
+						0.99 60.7%,
+						1 68.1%,
+						1.01 72.2%,
+						1 86.7%,
+						1 100%
+					);
+			}
+
+			&:hover {
+				transform: scale(1.2);
+			}
 
 			img {
 				width: 3rem;
