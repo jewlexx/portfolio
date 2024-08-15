5792cf7bb1459b57459e3151f3adc116873626df
Juliette Cordor <professional@maybejules.com>
2024-08-15 21:54:30 +1000

    add functions for getting posts

---
+++ src/content/posts.ts
@@ -0,0 +1,72 @@
+import fs from "fs";
+import { join } from "path";
+
+import matter from "gray-matter";
+
+const postsDirectory = join(process.cwd(), "src/content/projects");
+
+export interface Metadata {
+  featured?: boolean;
+  title?: string;
+  description?: string;
+  pubDate?: string;
+  repo?: string;
+  homepage?: string;
+  heroImage?: string;
+  shields?: Shield[];
+  toy?: boolean;
+  hideHero?: boolean;
+}
+
+export interface Shield {
+  alt?: string;
+  src?: string;
+  href?: string;
+}
+
+export interface PostInfo extends Metadata {
+  slug: string;
+  content: string;
+}
+
+export function getPostSlugs() {
+  return fs.readdirSync(postsDirectory);
+}
+
+export function getPostBySlug(slug: string): PostInfo {
+  const realSlug = slug.replace(/\.mdx?$/, "");
+  const fullPath = join(postsDirectory, slug);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+  const { data, content } = matter(fileContents);
+
+  return {
+    ...data,
+    slug: realSlug,
+    content,
+  };
+}
+
+export function getAllPosts() {
+  const slugs = getPostSlugs();
+  const posts = slugs
+    .map((slug) => getPostBySlug(slug))
+    .sort((a, b) => {
+      if (a.featured) {
+        return -1;
+      }
+      if (b.featured) {
+        return 1;
+      }
+
+      if (a.pubDate === undefined || b.pubDate === undefined) {
+        return 0;
+      }
+
+      if (new Date(a.pubDate) > new Date(b.pubDate)) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+  return posts;
+}
