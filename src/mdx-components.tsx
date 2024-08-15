3a21235c4b61fe297dcb357693931ce6624846d3
Juliette Cordor <professional@maybejules.com>
2024-08-15 17:44:06 +1000

    override a element in mdx components

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -2,6 +2,9 @@ import type { MDXComponents } from "mdx/types";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    a: (props) => {
+      return <a {...props} target="_blank" rel="noopener noreferrer" />;
+    },
     ...components,
   };
 }
