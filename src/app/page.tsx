5a9d3b2e168cda3a8039fbe8c17c81ea511b2725
Juliette Cordor <professional@maybejules.com>
2024-08-15 17:28:54 +1000

    updated emojis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
+import Image from "next/image";
+
+import Astronaut from "$/assets/images/emojis/astronaut.svg";
+
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.hero}>
       <div className={styles.overlay}>
-        <h1 className={styles.title}>🧑‍🚀Hello, Traveller!</h1>
+        <h1 className={styles.title}>
+          <Image src={Astronaut} alt="Astronaut Emoji" /> Hello, Traveller!
+        </h1>
         <p>
           My name is <span className={styles.name}>Juliette</span>, and you have
           landed on my <span className={styles.silly}>silly little site</span>
