167ea132bc1b699ce8c91de87f03f588bfe3c259
Juliette Cordor <professional@maybejules.com>
2024-08-16 12:24:32 +1000

    renamed postlink component

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ import { ProgressBarLink } from "$/components/ProgressBar";
 import { getAllPosts, sortPost } from "$/content/posts";
 
 import styles from "./page.module.scss";
-import PostDisplay from "$/components/PostDisplay";
+import PostDisplay from "$/components/PostLink";
 
 export default async function Projects() {
   const posts = getAllPosts().sort(sortPost);
