7cbc75b36c0d4723b392d3d9fcd00e56fe41f8ba
Juliette Cordor <professional@maybejules.com>
2024-08-18 18:26:26 +1000

    add glass section for overlay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,25 +7,23 @@ import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.hero}>
-      <div className={styles.overlay}>
-        <h1 className={styles.title}>
-          <Image src={Astronaut} alt="Astronaut Emoji" /> Hello, Traveller!
-        </h1>
-        <p>
-          My name is <span className={styles.name}>Juliette</span>, and you have
-          landed on my <span className={styles.silly}>silly little site</span>
-          &trade;.
-        </p>
-        <p>
-          Feel free to check out{" "}
-          <ProgressBarLink href="/projects">
-            some of my best projects
-          </ProgressBarLink>
-          , or return to the{" "}
-          <a href="https://www.google.com">ever-expansive web</a>.
-        </p>
-      </div>
-    </main>
+    <section className={styles.overlay}>
+      <h1 className={styles.title}>
+        <Image src={Astronaut} alt="Astronaut Emoji" /> Hello, Traveller!
+      </h1>
+      <p>
+        My name is <span className={styles.name}>Juliette</span>, and you have
+        landed on my <span className={styles.silly}>silly little site</span>
+        &trade;.
+      </p>
+      <p>
+        Feel free to check out{" "}
+        <ProgressBarLink href="/projects">
+          some of my best projects
+        </ProgressBarLink>
+        , or return to the{" "}
+        <a href="https://www.google.com">ever-expansive web</a>.
+      </p>
+    </section>
   );
 }
