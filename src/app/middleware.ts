6860efe7cc98c9a4ae342875150fdc938584b182
Juliette Cordor <professional@maybejules.com>
2024-08-19 16:45:16 +1000

    added middleware for contentful

---
+++ src/app/middleware.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request: {
+  url: string | URL;
+  headers: HeadersInit;
+}) {
+  const requestHeaders = new Headers(request.headers);
+
+  const referrer = requestHeaders.get("referer");
+
+  const response = NextResponse.next();
+
+  if (!referrer || !referrer.startsWith("https://app.contentful.com/")) {
+    response.headers.set("X-Frame-Options", "SAMEORIGIN");
+  }
+
+  return response;
+}
+
+export const config = {
+  matcher: "/:path*",
+};
