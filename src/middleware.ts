ae99a1dc9d7a89f2a3dc9438a2c63aa96fdab9a4
Juliette Cordor <professional@maybejules.com>
2024-08-19 17:43:54 +1000

    added unsafe-eval

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,9 +4,7 @@ export function middleware(request: NextRequest): NextResponse {
   const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
   const cspHeader = `
     default-src 'self';
-    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: 'unsafe-inline' ${
-      process.env.NODE_ENV === "production" ? "" : `'unsafe-eval'`
-    };
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic' https: http: 'unsafe-inline' 'unsafe-eval';
     style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
