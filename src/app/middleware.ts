0c1195f1040202e3e3e237dd65a4fd175d57c179
Juliette Cordor <professional@maybejules.com>
2024-08-19 16:51:24 +1000

    add csp to middleware

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,24 +1,42 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request: {
-  url: string | URL;
-  headers: HeadersInit;
-}) {
+export function middleware(request: NextRequest) {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
+    style-src 'self' 'nonce-${nonce}';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+    frame-ancestors https://app.contentful.com;
+`;
+  // Replace newline characters and spaces
+  const contentSecurityPolicyHeaderValue = cspHeader
+    .replace(/\s{2,}/g, " ")
+    .trim();
+
   const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
 
-  const referrer = requestHeaders.get("referer");
+  requestHeaders.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
 
-  const response = NextResponse.next();
-
-  if (!referrer || !referrer.startsWith("https://app.contentful.com/")) {
-    response.headers.set("X-Frame-Options", "DENY");
-  } else {
-    response.headers.delete("X-Frame-Options");
-  }
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+  response.headers.set(
+    "Content-Security-Policy",
+    contentSecurityPolicyHeaderValue
+  );
 
   return response;
 }
-
-export const config = {
-  matcher: "/:path*",
-};
