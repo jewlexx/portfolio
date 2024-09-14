596d3ab0cd2c2c649db2dd5fdf0e8482855332cb
Juliette Cordor <professional@maybejules.com>
2024-09-14 14:33:25 +1000

    added path revalidation

--- src/app/api/content/revalidate/route.ts
+++ src/app/api/content/revalidate/route.ts
@@ -1,4 +1,4 @@
-import { revalidateTag } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function POST(request: Request) {
   // idrk if this is super secure but whatever
@@ -8,6 +8,11 @@ export async function POST(request: Request) {
 
   if (secret === process.env.CONTENTFUL_WEBHOOKS_SECRET) {
     revalidateTag("posts");
+
+    revalidatePath("/rss.xml");
+    revalidatePath("/sitemap.xml");
+    revalidatePath("/blog");
+
     return new Response(null, {
       status: 200,
     });
