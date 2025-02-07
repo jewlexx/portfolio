7fe9dafe1f4cccda39fe7043a8a912469d54ba40
Juliette Cordor <me@cordor.dev>
2025-02-07 12:40:52 +1100

    updated project layout

---
+++ src/components/FormattedDate.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	interface $$Props {
+		date: Date;
+	}
+
+	const { date }: $$Props = $props();
+</script>
+
+<time dateTime={date.toISOString()}>
+	{date.toLocaleDateString('en-us', {
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric'
+	})}
+</time>
---
+++ src/components/Shield.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	interface $$Props {
+		enabled?: boolean;
+		href?: string;
+		// new_tab: boolean;
+		src?: string;
+		alt?: string;
+		class?: string;
+	}
+	const { enabled = true, href, src, alt, class: className }: $$Props = $props();
+</script>
+
+{#if enabled}
+	<a {href} target="_blank" rel="noopener noreferrer" class="mr-2 inline-block {className}">
+		<img {src} {alt} />
+	</a>
+{/if}
