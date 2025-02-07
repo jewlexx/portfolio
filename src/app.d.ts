3c7a6e55168015149f6533ce98cd30060aa1f24f
Juliette Cordor <me@cordor.dev>
2025-02-07 12:06:32 +1100

    AHH

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,9 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+export * from './lib/content/projects/types';
+import { Metadata } from './lib/content/projects/types';
+
 declare global {
 	namespace App {
 		// interface Error {}
@@ -11,3 +15,11 @@ declare global {
 }
 
 export {};
+
+declare module '*.md' {
+	import type { SvelteComponent } from 'svelte';
+
+	export default class Comp extends SvelteComponent {}
+
+	export const metadata: Metadata;
+}
