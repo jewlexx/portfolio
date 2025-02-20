403998ad118537474728c639033d5120ae293857
Juliette Cordor <me@cordor.dev>
2025-02-21 10:42:21 +1100

    Revert "Merge pull request #405 from jewlexx/svelte-kit"
    This reverts commit acf17b347bfc7115b0a6105844c6c07589021a30, reversing
    changes made to 23ea8d81f3d364875123b577b54b9323abddc456.

---
+++ src/links.ts
@@ -0,0 +1,59 @@
+import type React from "react";
+import {
+  IconBrandGithub,
+  IconBrandLinktree,
+  IconBrandTwitch,
+  IconBrandTwitter,
+  IconBrandBluesky,
+  IconRss,
+} from "@tabler/icons-react";
+
+import type IconLink from "./components/IconLink";
+
+export const links: React.ComponentProps<typeof IconLink>[] = [
+  {
+    title: "GitHub",
+    url: "github.com/jewlexx",
+    icon: IconBrandGithub,
+    className: "bg-black",
+  },
+  {
+    title: "Bluesky",
+    url: "bsky.app/profile/cordor.dev",
+    icon: IconBrandBluesky,
+    className: "bg-bluesky",
+  },
+  {
+    title: "Twitter",
+    url: "twitter.com/jewelexx",
+    icon: IconBrandTwitter,
+    className: "bg-twitter",
+  },
+  {
+    title: "Twitch",
+    url: "twitch.tv/digifem",
+    icon: IconBrandTwitch,
+    className: "bg-twitch",
+  },
+
+  {
+    title: "Linktree",
+    url: "linktr.ee/jewelexx",
+    icon: IconBrandLinktree,
+    className: "bg-linktree",
+  },
+  {
+    title: "RSS Feed",
+    url: "/rss.xml",
+    icon: IconRss,
+    className: "bg-rss",
+  },
+];
+
+// Subset of links to be displayed in the header
+export const linksAbridged: typeof links = [
+  links.find((link) => link.title === "GitHub")!,
+  links.find((link) => link.title === "Bluesky")!,
+  links.find((link) => link.title === "Linktree")!,
+  links.find((link) => link.title === "RSS Feed")!,
+];
--- src/params/projects.ts
+++
@@ -1,7 +0,0 @@
-import type { ParamMatcher } from '@sveltejs/kit';
-import type { ProjectName } from '../app';
-import { projectNames } from '$lib/content/projects';
-
-export const match = ((param: string): param is ProjectName => {
-	return projectNames.includes(param);
-}) satisfies ParamMatcher;
