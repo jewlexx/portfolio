c6ccd062dfb21a412aded220f8f5175e445caa34
Juliette Cordor <me@cordor.dev>
2025-02-21 10:53:33 +1100

    add unpin deps script

---
+++ scripts/unpinDeps.ts
@@ -0,0 +1,24 @@
+import * as pkg from "../package.json";
+
+const unpinDeps = (deps: Record<string, string>) => {
+  const unpinnedDeps = Object.entries(deps).reduce((acc, [name, version]) => {
+    if (version.startsWith("^")) {
+      return acc;
+    }
+
+    return { ...acc, [name]: `^${version}` };
+  }, {});
+
+  return unpinnedDeps;
+};
+
+const unpinnedDeps = unpinDeps(pkg.dependencies);
+const unpinnedDevDeps = unpinDeps(pkg.devDependencies);
+
+const newPkg = {
+  ...pkg,
+  dependencies: unpinnedDeps,
+  devDependencies: unpinnedDevDeps,
+};
+
+console.log(JSON.stringify(newPkg, null, 2));
