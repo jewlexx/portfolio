e06d8ce586d0de3a23f93eef43296b4634e90017
Juliette Cordor <me@cordor.dev>
2025-02-21 11:44:48 +1100

    added react compiler to eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,11 @@
 import { FlatCompat } from "@eslint/eslintrc";
 import { fixupConfigRules } from "@eslint/compat";
+import reactCompiler from "eslint-plugin-react-compiler";
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
+const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
 
 export default [
-  {
-    ignores: ["**/dev/*", "**/dist/*", "**/tests/*", "tsconfig.json"],
-  },
+  { ignores: ["**/dev/*", "**/dist/*", "**/tests/*", "tsconfig.json"] },
+  reactCompiler.configs.recommended,
   ...fixupConfigRules(compat.extends("next/core-web-vitals")),
 ];
