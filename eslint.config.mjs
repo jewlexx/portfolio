681a19f8b673cd17356d8721a927a346e320b745
Juliette Cordor <me@cordor.dev>
2025-02-21 11:53:54 +1100

    updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,27 @@
+// @ts-check
+
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import tseslint from "typescript-eslint";
+import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
-import { fixupConfigRules } from "@eslint/compat";
 import reactCompiler from "eslint-plugin-react-compiler";
 
-const compat = new FlatCompat({ baseDirectory: import.meta.dirname });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
-export default [
+export default tseslint.config([
   { ignores: ["**/dev/*", "**/dist/*", "**/tests/*", "tsconfig.json"] },
-  reactCompiler.configs.recommended,
-  ...fixupConfigRules(compat.extends("next/core-web-vitals")),
-];
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  ...compat.extends("next/core-web-vitals"),
+  {
+    plugins: { "react-compiler": reactCompiler },
+    rules: { "react-compiler/react-compiler": "error" },
+  },
+]);
