1d2c6bd91d2acf0a3e9aec13394e70af15bcf8d1
Juliette Cordor <me@cordor.dev>
2025-02-27 18:18:53 +1100

    updated draft mode handler for dev

--- src/app/api/draft/route.tsx
+++ src/app/api/draft/route.tsx
@@ -3,8 +3,13 @@ import { draftMode } from "next/headers";
 export async function GET() {
   if (process.env.NODE_ENV === "development") {
     const draft = await draftMode();
-    draft.enable();
-    return new Response("Draft mode is enabled");
+    if (draft.isEnabled) {
+      draft.disable();
+      return new Response("Draft mode is disabled");
+    } else {
+      draft.enable();
+      return new Response("Draft mode is enabled");
+    }
   } else {
     return new Response("Not supported in production environments");
   }
