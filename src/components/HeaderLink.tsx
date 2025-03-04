f02cbdc7f0e55923764ba5647f1da6243a001474
Juliette Cordor <me@cordor.dev>
2025-03-05 10:13:10 +1100

    removed client code from headerlink

--- src/components/HeaderLink.tsx
+++ src/components/HeaderLink.tsx
@@ -1,7 +1,4 @@
-"use client";
-
-import { AnchorHTMLAttributes, useMemo } from "react";
-import { usePathname } from "next/navigation";
+import type { AnchorHTMLAttributes } from "react";
 
 import { ProgressBarLink } from "$/components/ProgressBar";
 
@@ -10,16 +7,13 @@ export default function HeaderLink({
   children,
   ...props
 }: AnchorHTMLAttributes<HTMLAnchorElement> & { href: string }) {
-  const pathname = usePathname();
-  const isActive = useMemo(
-    () => href === pathname || href === pathname.replace(/\/$/, ""),
-    [href, pathname],
-  );
-
   return (
     <ProgressBarLink
       href={href}
-      className={`${props.className || ""} inline-block p-2 decoration-0 active:font-bold active:decoration-1 portrait:text-[3vw]`}
+      className={
+        props.className ||
+        "inline-block p-2 decoration-0 active:font-bold active:decoration-1 portrait:text-[3vw]"
+      }
       {...props}
     >
       {children}
