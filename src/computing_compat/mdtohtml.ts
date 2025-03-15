9a2486ce9c495fe50e59f97ee270ed723d6bf049
Juliette Cordor <me@cordor.dev>
2025-03-15 13:19:18 +1100

    ugh

--- src/computing_compat/mdtohtml.ts
+++ src/computing_compat/mdtohtml.ts
@@ -1,6 +1,6 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
+import remarkRehype, { defaultHandlers } from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
@@ -10,7 +10,46 @@ export const mdToHtml = async (markdown: string): Promise<string> => {
   const file = await unified()
     .use(remarkParse)
     .use(remarkGfm)
-    .use(remarkRehype)
+    .use(remarkRehype, {
+      handlers: {
+        ...defaultHandlers,
+        code: (state, node, parent) => {
+          const value = node.value ? node.value + "\n" : "";
+          /** @type {Properties} */
+          const properties = {};
+
+          if (node.lang) {
+            properties.className = ["language-" + node.lang];
+          }
+
+          // Create `<code>`.
+          /** @type {Element} */
+          let result = {
+            type: "element",
+            tagName: "code",
+            properties,
+            children: [{ type: "text", value }],
+          };
+
+          if (node.meta) {
+            result.data = { meta: node.meta };
+          }
+
+          state.patch(node, result);
+          result = state.applyData(node, result);
+
+          // Create `<pre>`.
+          result = {
+            type: "element",
+            tagName: "pre",
+            properties: {},
+            children: [result],
+          };
+          state.patch(node, result);
+          return result;
+        },
+      },
+    })
     .use(rehypeStringify)
     .use(rehypeHighlight)
     .process(markdown);
