972d01bf59f7c277d450a6146cdb9f862c4fbca3
Juliette Cordor <me@cordor.dev>
2025-03-18 18:42:33 +1100

    fixed eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,29 @@
 // @ts-check
 
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 import tseslint from "typescript-eslint";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
 import reactCompiler from "eslint-plugin-react-compiler";
-import reactHooks from "eslint-plugin-react-hooks";
 import pluginReact from "eslint-plugin-react";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
 
 const config = tseslint.config(
   tseslint.configs.recommended,
   tseslint.configs.stylistic,
   // @ts-expect-error React Compiler types mismatch tseslint types but are compatible
   reactCompiler.configs.recommended,
-  reactHooks.configs["recommended-latest"],
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
+  compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
       "**/dev/*",
