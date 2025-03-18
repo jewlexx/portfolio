6b07597c74fe596e5ebe60066bfa538be4411512
Juliette Cordor <me@cordor.dev>
2025-03-18 17:28:34 +1100

    got basic code blocks working

--- src/computing_compat/mdtohtml.ts
+++ src/computing_compat/mdtohtml.ts
@@ -1,9 +1,13 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
-import remarkRehype, { defaultHandlers } from "remark-rehype";
+import type { Handler } from "mdast-util-to-hast";
+import type { Code } from "mdast";
+import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
 import remarkGfm from "remark-gfm";
 import rehypeHighlight from "rehype-highlight";
+
+type HandlerResult = ReturnType<Handler>;
 
 // TODO: Use custom plugin to parse code
 export const mdToHtml = async (markdown: string): Promise<string> => {
@@ -12,38 +16,47 @@ export const mdToHtml = async (markdown: string): Promise<string> => {
     .use(remarkGfm)
     .use(remarkRehype, {
       handlers: {
-        ...defaultHandlers,
-        code: (state, node, parent) => {
+        code: (state, node: Code) => {
           const value = node.value ? node.value + "\n" : "";
-          /** @type {Properties} */
-          const properties = {};
+          const values = value.split("\n");
 
-          if (node.lang) {
-            properties.className = ["language-" + node.lang];
-          }
+          const valueElements = values.map((value) => {
+            const className = [];
+            if (node.lang) {
+              className.push(`language-${node.lang}`);
+            }
+            let result: HandlerResult = {
+              type: "element",
+              tagName: "code",
+              properties: { className },
+              children: [{ type: "text", value }],
+            };
 
-          // Create `<code>`.
-          /** @type {Element} */
-          let result = {
+            if (node.meta) {
+              result.data = { meta: node.meta };
+            }
+
+            state.patch(node, result);
+            result = state.applyData(node, result);
+
+            // Create `<pre>`.
+            result = {
+              type: "element",
+              tagName: "pre",
+              properties: {},
+              children: [result],
+            };
+            state.patch(node, result);
+
+            result = state.applyData(node, result);
+            return result;
+          });
+
+          const result: HandlerResult = {
             type: "element",
-            tagName: "code",
-            properties,
-            children: [{ type: "text", value }],
-          };
-
-          if (node.meta) {
-            result.data = { meta: node.meta };
-          }
-
-          state.patch(node, result);
-          result = state.applyData(node, result);
-
-          // Create `<pre>`.
-          result = {
-            type: "element",
-            tagName: "pre",
-            properties: {},
-            children: [result],
+            tagName: "div",
+            properties: { className: ["computing-code"] },
+            children: valueElements,
           };
           state.patch(node, result);
           return result;
