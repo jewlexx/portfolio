9aaa66a98066ee648ebb5961a4b8f5bc450a9cea
Juliette Cordor <me@cordor.dev>
2025-11-22 20:45:00 +1100

    fixed zod-validation-error problem

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,42 +1,32 @@
 // @ts-check
 
-import path from "node:path";
-import { fileURLToPath } from "node:url";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
 import reactCompiler from "eslint-plugin-react-compiler";
 import pluginReact from "eslint-plugin-react";
+import webVitals from "eslint-config-next";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
-
-const config = tseslint.config(
+const config = defineConfig(
+  ...webVitals,
   tseslint.configs.recommended,
   tseslint.configs.stylistic,
   reactCompiler.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
-  compat.extends("plugin:@next/next/recommended"),
-  {
-    settings: {
-      react: {
-        version: "detect",
-      },
-    },
-    ignores: [
-      "**/dev/*",
-      "**/dist/*",
-      "**/tests/*",
-      "tsconfig.json",
-      "src/lib/paraglide/*",
-    ],
-  },
+  // {
+  //   settings: {
+  //     react: {
+  //       version: "detect",
+  //     },
+  //   },
+  //   ignores: [
+  //     "**/dev/*",
+  //     "**/dist/*",
+  //     "**/tests/*",
+  //     "tsconfig.json",
+  //     "src/lib/paraglide/*",
+  //   ],
+  // },
 );
 
 export default config;
