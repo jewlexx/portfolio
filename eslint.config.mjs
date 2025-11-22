c2ec7ffb6f37e6ee6fd9e7feca37ab4202fa6f7f
Juliette Cordor <me@cordor.dev>
2025-11-22 20:45:29 +1100

    restore eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,29 +4,31 @@ import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
 import reactCompiler from "eslint-plugin-react-compiler";
 import pluginReact from "eslint-plugin-react";
-import webVitals from "eslint-config-next";
+import webVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
 
 const config = defineConfig(
   ...webVitals,
+  ...nextTs,
   tseslint.configs.recommended,
   tseslint.configs.stylistic,
   reactCompiler.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat["jsx-runtime"],
-  // {
-  //   settings: {
-  //     react: {
-  //       version: "detect",
-  //     },
-  //   },
-  //   ignores: [
-  //     "**/dev/*",
-  //     "**/dist/*",
-  //     "**/tests/*",
-  //     "tsconfig.json",
-  //     "src/lib/paraglide/*",
-  //   ],
-  // },
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    ignores: [
+      "**/dev/*",
+      "**/dist/*",
+      "**/tests/*",
+      "tsconfig.json",
+      "src/lib/paraglide/*",
+    ],
+  },
 );
 
 export default config;
