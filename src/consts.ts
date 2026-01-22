c34cdaa1c62087d2ceec8ad996b46b03d570abad
Juliette Cordor <me@cordor.dev>
2026-01-22 19:01:58 +1100

    moved header to .astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-
 export const SITE_TITLE = "Juliette Cordor's silly little universe";
 export const SITE_DESCRIPTION =
   "Juliette Cordor's section of the internet. Juliette is a developer, among other things, but mostly that.";
@@ -10,7 +9,7 @@ export const twitterConfiguration = {
   card: "summary_large_image",
   creator: "@jewlexx",
   site: "https://cordor.dev",
-}as const;
+} as const;
 
 export const BASE_URL =
   process.env.NODE_ENV === "production"
