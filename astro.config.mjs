a518bc336493fab7c0d68a9479e5ce6e155d1ec1
Juliette Cordor <me@cordor.dev>
2026-01-23 19:55:29 +1100

    fixed classes

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.cordor.dev",
-  trailingSlash: "always",
 
   integrations: [react()],
   vite: {
