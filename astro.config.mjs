7ab3748411d9f25591a7023b0976b1bf2694efdc
Juliette Cordor <me@cordor.dev>
2026-01-23 20:06:29 +1100

    hm

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://www.cordor.dev",
-
+  prefetch: true,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
