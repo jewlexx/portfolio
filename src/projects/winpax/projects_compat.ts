5c69610319283ba4ea02a7d44424a3cc6b2f513e
Juliette Cordor <me@cordor.dev>
2026-01-26 09:15:59 +1100

    updated winpax info

--- src/projects/winpax/projects_compat.ts
+++ src/projects/winpax/projects_compat.ts
@@ -1,21 +1,20 @@
-import type { Project } from "$/projects";
+import { projectSchema, type Project } from "$/projects";
 
 export default function appendProject(
   imageUrl: string,
   projects: Project[],
 ): Project[] {
-  return [
-    ...projects,
-    {
-      emoji: "📦",
-      slug: "winpax",
-      content: "",
-      title: "Winpax",
-      description:
-        "Creating blazing fast, package management solutions for the Windows platform.",
-      // aproximate date
-      pubDate: "",
-      heroImage: imageUrl,
-    },
-  ];
+  const winpaxInfo = projectSchema.parse({
+    emoji: "📦",
+    slug: "winpax",
+    content: "",
+    title: "Winpax",
+    description:
+      "Creating blazing fast, package management solutions for the Windows platform.",
+    // aproximate date
+    pubDate: "",
+    heroImage: imageUrl,
+  });
+
+  return [...projects, winpaxInfo];
 }
